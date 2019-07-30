83fef799ceed77fa340a4a5f1f4b5152a839ea89
sunli <sunli@travelsky.com>
2019-07-30 21:20:03 +0800

    v1.0.0

--- packages/dialog/index.js
+++
@@ -1,9 +0,0 @@
-import Alert from './alert/src/alert.js'
-import Confirm from './confirm/src/confirm.js'
-import Propmt from './prompt/src/prompt.js'
-
-export default {
-  alert: Alert,
-  confirm: Confirm,
-  prompt: Propmt
-}
